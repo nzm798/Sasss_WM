21f3bf855abe3760d7ca037fdbfbb22510542134
nzm798 <798736800@qq.com>
2023-03-30 16:05:56 +0800

    0.0.1-SNAPSHOT-Sasss_WM

---
+++ src/main/java/com/ss/SasssWmApplication.java
@@ -0,0 +1,13 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasssWmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasssWmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/SasssWmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasssWmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
