fe853aa5ef3fc1ca830fa1ac99af22a22035c417
nzm798 <798736800@qq.com>
2023-04-04 11:24:40 +0800

    0.0.1-SNAPSHOT-Sasss_WM

---
+++ src/main/java/com/ss/exception/DataAccessException.java
@@ -0,0 +1,25 @@
+package com.ss.exception;
+
+public class DataAccessException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public DataAccessException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public DataAccessException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public DataAccessException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+}
